cf70114c57d9597fd77f9d62409abf547cd00477
sergiocorato <sergiocorato@gmail.com>
2020-01-04 23:29:54 +0100

    [UPD]

--- l10n_it_account_stamp_ddt/__manifest__.py
+++ l10n_it_account_stamp_ddt/__manifest__.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Alex Comba - Agile Business Group
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Italian Localization - Imposta di bollo - DDT',
     'summary': "Modulo ponte tra imposta di bollo e DDT",
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Localization/Italy',
     'license': 'AGPL-3',
     'author': 'Agile Business Group, Odoo Community Association (OCA)',
     'maintainers': ['tafaRU'],
-    'website': 'https://github.com/OCA/l10n-italy/tree/10.0/'
-               'l10n_it_account_stamp_ddt',
+    'website': 'https://github.com/OCA/l10n-italy',
     'depends': [
         'l10n_it_account_stamp',
         'l10n_it_ddt',
